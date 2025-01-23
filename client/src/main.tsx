b97394a3b6c726ac2c8d592447db9ba753e30c2d
Matthew Kennedy <mkennedy.akgrown@gmail.com>
2025-01-22 18:33:37 -0600

    Add Pages and Components directories, ErrorPage, Home, Login, Reports, routes

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import routes from "./routes";
+import "./index.css";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={createBrowserRouter(routes)} />
+  </StrictMode>
+);
---
+++ client/src/routes.tsx
@@ -0,0 +1,21 @@
+import App from "./App.tsx";
+import Home from "./Pages/Home.tsx";
+import Login from "./Pages/Login.tsx";
+import Reports from "./Pages/Reports.tsx";
+import ErrorPage from "./Pages/ErrorPage.tsx";
+
+const routes = [
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      { path: "/", element: <Home /> },
+      { path: "/login", element: <Login /> },
+      { path: "/reports", element: <Reports /> },
+      { path: "*", element: <ErrorPage /> },
+    ],
+  },
+];
+
+export default routes;
