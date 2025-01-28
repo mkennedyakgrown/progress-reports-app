49b987f7dd5c9999f93bcb113a248e757ddcc977
Matthew Kennedy <mkennedy.akgrown@gmail.com>
2025-01-28 14:00:01 -0600

    Add undo/redo buttons and functionality to CourseReport

--- client/src/Components/CourseReport.tsx
+++ client/src/Components/CourseReport.tsx
@@ -1,10 +1,12 @@
-import { ListItem, Box, ListItemText } from "@mui/material";
+import { ListItem, Box, ListItemText, Button } from "@mui/material";
 import { useRef, useState } from "react";
 import ReportTextField from "./ReportTextField";
 
 function CourseReport({ course, handleUpdateCourseReport }) {
   const [reportText, setReportText] = useState(course.report_text);
+  const [undoStack, setUndoStack] = useState([course.report_text]);
   const { current } = useRef({ reportText, timer: 0 });
+  const undoStackPointer = useRef(0);
 
   function handleUpdateRequest(currentReportText) {
     console.log("Updating course report...", course.id);
@@ -24,10 +26,8 @@ function CourseReport({ course, handleUpdateCourseReport }) {
       .catch((error) => console.error("Error patching course report:", error));
   }
 
-  function handleTextChange(
-    event: React.ChangeEvent<HTMLTextAreaElement | HTMLInputElement>
-  ) {
-    setReportText(event.target.value);
+  function handleTextChange(currentReportText: string) {
+    setReportText(currentReportText);
 
     if (current.timer) {
       clearTimeout(current.timer);
@@ -35,8 +35,40 @@ function CourseReport({ course, handleUpdateCourseReport }) {
 
     current.timer = setTimeout(() => {
       current.timer = 0;
-      handleUpdateRequest(event.target.value);
+
+      // Check if undoTextStackPointer is pointing to earlier version of text
+      if (undoStackPointer.current < undoStack.length - 1) {
+        // If so, check if the text is different from the current text
+        if (undoStack[undoStackPointer.current] != currentReportText) {
+          // If text is different, slice the stack to current version and then add the new text, then increment the pointer
+          setUndoStack([
+            ...undoStack.slice(0, undoStackPointer.current + 1),
+            currentReportText,
+          ]);
+          undoStackPointer.current = undoStackPointer.current + 1;
+        }
+      } else {
+        // If pointer is at the end of the stack, add the new text and increment the pointer
+        setUndoStack([...undoStack, currentReportText]);
+        undoStackPointer.current = undoStackPointer.current + 1;
+      }
+
+      handleUpdateRequest(currentReportText);
     }, 5000);
+  }
+
+  function handleUndo() {
+    if (undoStackPointer.current > 0) {
+      undoStackPointer.current -= 1;
+      handleTextChange(undoStack[undoStackPointer.current]);
+    }
+  }
+
+  function handleRedo() {
+    if (undoStackPointer.current < undoStack.length - 1) {
+      undoStackPointer.current += 1;
+      handleTextChange(undoStack[undoStackPointer.current]);
+    }
   }
 
   return (
@@ -50,6 +82,12 @@ function CourseReport({ course, handleUpdateCourseReport }) {
             reportType: "Course",
           }}
         />
+        <Button type="button" onClick={handleUndo}>
+          Undo
+        </Button>
+        <Button type="button" onClick={handleRedo}>
+          Redo
+        </Button>
       </Box>
     </ListItem>
   );
